c03c82c434c6c0b625776696529dbeac8fea5037
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-10-15 22:00:59 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
+input.onButtonEvent(Button.B, btf.buttonEventValue(ButtonEvent.Hold), function () {
+    btf.buttonBhold()
+})
 btf.onReceivedDataChanged(function (receivedData, changed) {
-    receiver.dual2MotorenLenkenBuffer(receivedData, 50)
+    receiver.dual2MotorenLenkenBuffer(receivedData, 30)
+    btf.zeige5x5Buffer(receivedData)
+    btf.zeige5x5Joystick(receivedData)
     btf.setLedColors(btf.btf_RgbLed(btf.eRgbLed.a), 0x0000ff, true, true)
+    btf.setLedColors(btf.btf_RgbLed(btf.eRgbLed.b), 0xffffff, receiver.pinSpurrechts(receiver.eDH.hell))
+    btf.setLedColors(btf.btf_RgbLed(btf.eRgbLed.c), 0xffffff, receiver.pinSpurlinks(receiver.eDH.hell))
+})
+input.onButtonEvent(Button.A, btf.buttonEventValue(ButtonEvent.Hold), function () {
+    btf.buttonAhold()
 })
 receiver.beimStart(
 receiver.eHardware.v3,
