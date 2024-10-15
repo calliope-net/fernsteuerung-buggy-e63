edf0d7ffe22e0d89c2c85ea30a3b49201f4c6ddc
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-10-15 13:11:27 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
+btf.onReceivedDataChanged(function (receivedData, changed) {
+    receiver.dual2MotorenLenkenBuffer(btf.btf_receivedBuffer19(), 50)
+    btf.setLedColors(btf.btf_RgbLed(btf.eRgbLed.a), 0x0000ff, true, true)
 })
+receiver.beimStart(
+receiver.eHardware.v3,
+90,
+true,
+65
+)
