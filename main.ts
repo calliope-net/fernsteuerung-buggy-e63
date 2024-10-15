6f601f47c7fee5263f345e9ec57dda82172f8a16
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-10-15 18:48:54 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,3 +8,13 @@ receiver.eHardware.v3,
 true,
 65
 )
+loops.everyInterval(700, function () {
+    if (btf.timeout(30000, true)) {
+        receiver.pinRelay(false)
+    } else if (btf.timeout(1000)) {
+        btf.setLedColors(btf.btf_RgbLed(btf.eRgbLed.a), 0xff0000, true, true)
+        receiver.dualMotor128(receiver.eDualMotor.M0_M1, 128)
+    } else if (btf.timeout(1000, true)) {
+        btf.setLedColors(btf.btf_RgbLed(btf.eRgbLed.a), 0x00ff00)
+    }
+})
