840a0f50916694f2f72efa6f4fdb52f79e3807cd
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-10-17 19:19:14 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,46 @@
 receiver.onSpurEvent(function (links_hell, rechts_hell, abstand_Stop) {
     receiver.buffer_Spur_folgen(btf.btf_receivedBuffer19(), links_hell, rechts_hell, abstand_Stop)
+    receiver.event_Spur_folgen(
+    receiver.isFunktion(receiver.eFunktion.spur_folgen),
+    links_hell,
+    rechts_hell,
+    192,
+    160,
+    31,
+    0,
+    abstand_Stop,
+    cb2.cb2_zehntelsekunden(btf.ePause.s1)
+    )
+})
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    if (receiver.isFunktion(receiver.eFunktion.ng)) {
+        receiver.setFunktion(receiver.eFunktion.hindernis_ausweichen)
+    } else if (receiver.isFunktion(receiver.eFunktion.hindernis_ausweichen)) {
+        receiver.setFunktion(receiver.eFunktion.ng)
+    } else if (receiver.isFunktion(receiver.eFunktion.spur_folgen)) {
+        Ultraschall_Sensor_Knopf_A = !(Ultraschall_Sensor_Knopf_A)
+    }
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    if (receiver.isFunktion(receiver.eFunktion.ng)) {
+        Ultraschall_Sensor_Knopf_A = true
+        receiver.setFunktion(receiver.eFunktion.spur_folgen)
+    } else if (receiver.isFunktion(receiver.eFunktion.spur_folgen)) {
+        Ultraschall_Sensor_Knopf_A = false
+        receiver.setFunktion(receiver.eFunktion.ng)
+    }
 })
 receiver.onAbstandEvent(function (abstand_Sensor, abstand_Stop, cm) {
     receiver.buffer_Hindernis_ausweichen(btf.btf_receivedBuffer19(), abstand_Stop)
+    receiver.event_Hindernis_ausweichen(
+    receiver.isFunktion(receiver.eFunktion.hindernis_ausweichen),
+    abstand_Stop,
+    255,
+    16,
+    64,
+    0,
+    cb2.cb2_zehntelsekunden(btf.ePause.s1)
+    )
     if (abstand_Stop) {
         btf.setLedColors(btf.btf_RgbLed(btf.eRgbLed.b), 0xff0000)
     } else {
@@ -21,11 +59,15 @@ btf.onReceivedDataChanged(function (receivedData, changed) {
 input.onButtonEvent(Button.A, btf.buttonEventValue(ButtonEvent.Hold), function () {
     btf.buttonAhold()
 })
+let Ultraschall_Sensor_Knopf_A = false
 receiver.beimStart2Motoren()
+Ultraschall_Sensor_Knopf_A = false
 basic.forever(function () {
-    receiver.buffer_raiseAbstandMotorStop(btf.btf_receivedBuffer19())
+    receiver.buffer_raiseAbstandMotorStop(btf.btf_receivedBuffer19(), true)
     receiver.buffer_raiseAbstandEvent(btf.btf_receivedBuffer19())
     receiver.buffer_raiseSpurEvent(btf.btf_receivedBuffer19())
+    receiver.raiseAbstandEvent(receiver.isFunktion(receiver.eFunktion.hindernis_ausweichen) || Ultraschall_Sensor_Knopf_A, 30, 35)
+    receiver.raiseSpurEvent(receiver.isFunktion(receiver.eFunktion.spur_folgen))
 })
 loops.everyInterval(700, function () {
     if (btf.timeout(30000, true)) {
